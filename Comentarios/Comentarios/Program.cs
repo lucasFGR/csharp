e25ca223281e9b76d47e34648358633458c21e58
Lucas Gonçalves <lucasg@emphasisconf.com.br>
2022-08-05 15:46:43 -0300

    Estrutura de um programa C# e Comentários no código

---
+++ Comentarios/Comentarios/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comentarios
+{
+    internal class Program
+    {
+        // Metodo principal
+        // Outra linha
+        static void Main(string[] args)
+        {
+            /*
+                Esse é 
+                um bloco de 
+                Comentário
+                Posso inserir várias linhas
+            */
+
+            #region Primeira Parte
+            Console.WriteLine("Primeira parte");
+            #endregion
+
+            #region Segunda Parte
+            Console.WriteLine("Segunda parte");
+            #endregion
+
+
+            Console.ReadKey();
+        }
+    }
+}
