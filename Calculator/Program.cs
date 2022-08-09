204fff0193713d3babb70bcd5be3098cdc346130
Lucas Gonçalves <lucasg@emphasisconf.com.br>
2022-08-09 16:48:10 -0300

    Iniciando o projeto

---
+++ Calculator/Program.cs
@@ -0,0 +1,45 @@
+﻿
+// Limpando o console
+
+Soma();
+Subtracao();
+
+static void Soma()
+{
+    Console.Clear();
+
+    Console.WriteLine("Primeiro valor:");
+
+    // Convertendo o valor que vem do ReadLine para float 
+    float v1 = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("Segundo valor:");
+    float v2 = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("");
+
+    float resultado = v1 + v2;
+
+    Console.WriteLine($"O resultado da soma é: {resultado}");
+}
+
+static void Subtracao()
+{
+    Console.Clear();
+
+    Console.WriteLine("Primeiro valor:");
+
+    // Convertendo o valor que vem do ReadLine para float 
+    float v1 = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("Segundo valor:");
+    float v2 = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("");
+
+    float resultado = v1 - v2;
+
+    Console.WriteLine($"O resultado da subtracao é: {resultado}");
+}
+
+
