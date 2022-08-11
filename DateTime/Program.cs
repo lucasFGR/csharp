f1cf81dcd2ba6533e5d428260e9dfb68d79f0a47
zeer0e1 <lucasfreitasr@outlook.com>
2022-08-11 20:57:29 -0300

    Estudando sobre data em c#

---
+++ DateTime/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Globalization;
+var data = DateTime.Now;
+
+// Y para exibir ano
+// M para mes
+// D dia
+// h hora
+// s segundos
+/*
+var dtFormatada = String.Format("{0:yyyy}", data);
+var dtFormatada2 = String.Format("{0:dd/MM/yyyy HH:mm:ss}", data);
+*/
+
+//Somando datas
+
+Console.Clear();
+
+Console.WriteLine(data.AddDays(12));
+Console.WriteLine(data.AddMonths(1));
+Console.WriteLine(data.AddHours(2));
+
+
+// Comparando datas
+
+if (data.Date == DateTime.Now.Date)
+{
+    Console.WriteLine("É igual");
+}
+Console.WriteLine(data);
+
+
+// Culture Info
+var pt = new CultureInfo("pt-BR");
+Console.WriteLine(DateTime.Now.ToString("D", pt));
+
+// Timezone
+var dataLocal = DateTime.UtcNow;
+Console.WriteLine(dataLocal);
+
+//Timezone
+var timeSpan = new TimeSpan();
+Console.WriteLine(timeSpan);
+
+var timeSpanNanoSegundos = new TimeSpan(1);
+Console.WriteLine(timeSpanNanoSegundos);
+
+var timeSpanHoMinutoraSegundo = new TimeSpan(5, 12, 8);
+Console.WriteLine(timeSpanHoMinutoraSegundo);
+
