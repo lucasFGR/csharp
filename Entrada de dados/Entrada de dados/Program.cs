65654bd0945e3484a68670ada6f60e0c982e57bd
zeer0e1 <lucasfreitasr@outlook.com>
2022-08-13 18:09:59 -0300

    Entrada de dados

---
+++ Entrada de dados/Entrada de dados/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entrada_de_dados
+{
+    internal class Program
+    {
+
+        struct Pessoa
+         {
+            public string nome;
+            public string sobrenome;
+            public int idade;
+            public float peso;
+            public float altura;
+         }
+        static void Main(string[] args)
+        {
+            Pessoa pessoa1 = new Pessoa();
+            
+            Console.WriteLine("Digite o seu nome");
+            pessoa1.nome = Console.ReadLine();
+
+            Console.WriteLine("Digite o seu sobrenome");
+            pessoa1.sobrenome = Console.ReadLine();
+
+            Console.WriteLine($"Nome:{pessoa1.nome} \n Sobrenome: {pessoa1.sobrenome}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Moedas/Program.cs
@@ -0,0 +1,6 @@
+﻿using System.Globalization;
+decimal valor = 10.25m;
+Console.WriteLine(valor.ToString("C", CultureInfo.CreateSpecificCulture("pt-BR")));
+Console.WriteLine(Math.Round(valor));
+Console.WriteLine(Math.Ceiling(valor)); //Arredonda para cima 
+Console.WriteLine(Math.Floor(valor)); // Arrendo para baixo
